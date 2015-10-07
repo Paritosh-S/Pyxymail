76baaeef9103e4bff11ff5fd13cdf693961fa854
Paritosh-S <paritosh.sawant@vulcaninfotech.com>
2015-10-07 11:06:19 +0530

    no message

--- about.php
+++ about.php
@@ -4,7 +4,6 @@
     <head>
         <meta charset="utf-8">
         <title>About Us</title>
-        <meta name="description" content="">
         <meta name="viewport" content="width=device-width">
 
        
